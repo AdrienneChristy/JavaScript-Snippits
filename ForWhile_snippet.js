a14b4bf64a5aa6cebc1c6dab2ba9c30913020553
AdrienneChristy <AdrienneChristy@users.noreply.github.com>
2014-05-20 17:06:26 -0600

    Create ForWhile_snippet.js

---
+++ ForWhile_snippet.js
@@ -0,0 +1,13 @@
+for (var i = 1; i < 11; i++) {
+    // Prints numbers 1 - 10
+    console.log(i);
+}
+
+
+var condition = true;
+while(condition) {
+    // Says "Hello!"
+    console.log("Hello!");
+    // Avoid infinite loops
+    condition = false;
+}
