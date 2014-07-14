0d375ebcf611fd4b723faf52b6fbfebdf64564a3
AdrienneChristy <AdrienneChristy@users.noreply.github.com>
2014-07-14 11:27:37 -0600

    Create objects_literalVconstructor.js

---
+++ objects_literalVconstructor.js
@@ -0,0 +1,12 @@
+// new object using literal notation
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// new object using constructor notation
+var spencer = new Object(){
+    spencer.age = 22;
+    spencer.country = "United States";
+}
+    
