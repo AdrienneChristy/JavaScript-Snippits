20dc06b36d28cbb8612dd7fad37074b45caa9017
AdrienneChristy <AdrienneChristy@users.noreply.github.com>
2014-07-14 18:12:30 -0600

    Create _propertiesAndPassword.js

---
+++ _propertiesAndPassword.js
@@ -0,0 +1,20 @@
+//Public properties and one private property, with a public function and password requirement to access
+
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+  
+   this.askTeller = function(pass) {
+     if (pass == 1234) return bankBalance;
+     else return "Wrong password.";
+   };
+}
+
+var john = new Person('John','Smith', 30);
+/* the variable myBalance should access askTeller()
+   with a password as an argument  */
+ 
+var myBalance = john.askTeller(1234);
+console.log(myBalance);
